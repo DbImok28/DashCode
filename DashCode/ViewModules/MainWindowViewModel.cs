b8f4850747146f0e82c606934a566a1e15144224
RubyCircle <pavel.jakushik@yandex.by>
2022-03-31 19:38:44 +0300

    Добавлены основные файлы

---
+++ DashCode/ViewModules/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DashCode.ViewModules
+{
+    internal class MainWindowViewModel : BaseViewModel
+    {
+        #region Title
+        private string _Title = "DashCode";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+        #region FontSize
+        private int _FontSize = 14;
+        public int FontSize
+        {
+            get => _FontSize;
+            set => Set(ref _FontSize, value);
+        }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
